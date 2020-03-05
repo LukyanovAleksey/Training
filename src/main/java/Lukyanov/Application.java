42bba02fb527069ec9c1f2de5cbc65d4d61d669b
LukyanovAleksey <61530873+LukyanovAleksey@users.noreply.github.com>
2020-03-05 15:46:18 +0400

    Update Application.java

--- src/main/java/Lukyanov/Application.java
+++ src/main/java/Lukyanov/Application.java
@@ -1,12 +1,24 @@
 package Lukyanov;
 
+import Lukyanov.Human.HumanDTO;
+
+import java.util.List;
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Random random = new Random();
+        HumanService service = new HumanService();
+        HumanDTO humanDTO = service.getEntity(random.nextLong());
+        List<HumanDTO> listOfHumanDTO = service.getAllEntities();
+        service.saveEntity(humanDTO);
+        service.saveAllEntities(listOfHumanDTO);
+        System.out.println("All tasks have been finished!");
     }
 }
 
 /*
+Задание:
 Создать доменную сущность для БД – Human со следующим набором полей:
 - Id;
 - name;
