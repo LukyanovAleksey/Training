c810575893aedef99ae81995f69c0c34da7d4ec2
a.lukyanov <a.lukyanov92@yandex.ru>
2020-04-05 20:29:59 +0400

    Added Human pojo class and custom Annotation Value

---
+++ src/main/java/lukyanov/Human.java
@@ -0,0 +1,28 @@
+package lukyanov;
+
+public class Human {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{age=" + age +
+                ", name='" + name + '\'' + '}';
+    }
+}
